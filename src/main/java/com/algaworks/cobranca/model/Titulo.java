664bef47e86babe0973ff12148d7a0fb4bd25cd8
Rapharocha <rapharocha2015@hotmail.com>
2020-01-28 15:02:01 -0300

    quarto dia concluido

--- src/main/java/com/algaworks/cobranca/model/Titulo.java
+++ src/main/java/com/algaworks/cobranca/model/Titulo.java
@@ -11,6 +11,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
@@ -29,14 +35,19 @@ public class Titulo {
 	private long codigo;
 	
 	
+	
+	@NotEmpty(message = "Descrição é obrigatória")
+	@Size(max = 60, message = "A descrição não pode conter mais de 60 caracteres")
 	private String descricao;
 	
-	
+	@NotNull(message = "Data de vencimento é obrigatória ") 
 	@DateTimeFormat(pattern="dd/MM/yyyy")//Para definir formato da data
 	@Temporal(TemporalType.DATE) //Para Salvar apenas a data
 	private Date dataVencimento;
 	
-	
+	@NotNull(message ="Valor é Obrigatório")
+	@DecimalMin(value = "0.01", message = "Valor não pode ser menor que 0,01")
+	@DecimalMax(value = "9999999999.99", message = "Valor não pode ser maior que 9.999.999.999,99" )
 	@NumberFormat(pattern="#,###.00")
 	private BigDecimal valor;
 	
